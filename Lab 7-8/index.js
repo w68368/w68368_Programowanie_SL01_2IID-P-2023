6a6bca8b503581988d761d2c4a148bd8c0014ed4
Anastasiya <nastia.bel04@gmail.com>
2023-04-28 14:55:43 +0200

    lab78

--- Lab 7-8/index.js
+++ Lab 7-8/index.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-})
+});
 
 const nameField = document.querySelector('[name="name"]');
 
