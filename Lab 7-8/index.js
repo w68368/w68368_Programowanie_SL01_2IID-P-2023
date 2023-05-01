bc1456369ae1f085cfc2956716b7dfa7b0fa5da6
Anastasiya <nastia.bel04@gmail.com>
2023-05-01 20:18:38 +0200

    lab7

--- Lab 7-8/index.js
+++ Lab 7-8/index.js
@@ -1,11 +1,15 @@
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-});
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+// });
 
-const nameField = document.querySelector('[name="name"]');
+// const nameField = document.querySelector('[name="name"]');
 
-nameField.addEventListener('input', () => {
-    console.log(nameField.value);
-});
+// nameField.addEventListener('input', () => {
+//     console.log(nameField.value);
+// });
+
+let liczba1 = prompt("wpisz liczbe 1", "liczba 1");
+let liczba2 = prompt("wpisz liczbe 2", "liczba 2");
+document.write(liczba1 + liczba2);
