dff7b70cfed4afb28a86aed3a2975f6dc3131e66
Anastasiya <nastia.bel04@gmail.com>
2023-05-03 15:38:04 +0200

    lab3

--- Lab 7-8/index.js
+++ Lab 7-8/index.js
@@ -10,21 +10,3 @@
 //     console.log(nameField.value);
 // });
 
-let liczba1 = prompt("podaj liczbe 1:", "liczba 1");
-let liczba2 = prompt("podaj liczbe 2:", "liczba 2");
-let operator = prompt("podaj + - * /:", "operator");
-if (operator == "+") {
-    alert(liczba1 + liczba2);
-}
-if (operator == "-") {
-    alert(liczba1 - liczba2);
-}
-if (operator == "*") {
-    alert(liczba1 * liczba2);
-}
-if (operator == "/") {
-    alert(liczba1 / liczba2);
-}
-else {
-    alert("bledny operator")
-}
