3da7d399ff91d1bff27db6dcaae683d6403ad2aa
Anastasiya <nastia.bel04@gmail.com>
2023-05-03 16:21:42 +0200

    lab4

--- Lab 4/index.js
+++ Lab 4/index.js
@@ -1,12 +1,95 @@
-function Book(tutyl, autor, rok) {
-    this.tutyl = tutyl;
-    this.autor = autor;
-    this.rok = rok;
-    this.fullBook = function(){
-        return this.tutyl + "-" + this.autor + "(" + this.rok + ")";
+// Zadanie 1
 
-    };
-    
-   }
-const ksiazka = new Book("Wojna i pokoj", "Lew Tolstoj", 1863);
-console.log(ksiazka.fullBook())
+const book = {
+    title: "Harry Potter i kamień filozoficzny",
+    author: "Joannę Rowling",
+    year: 1997,
+  };
+  
+  function getBookInfo(book) {
+    return `${book.title} - ${book.author} (${book.year})`;
+  }
+  
+  // Zadanie 2
+  
+  const bookInfo = getBookInfo(book);
+  
+  console.log(bookInfo);
+  
+  const student = {
+    firstName: "",
+    lastName: "",
+    albumNumber: "68153",
+    grades: [4, 4, 5],
+  };
+  
+  const averageGrade =
+    student.grades.reduce((sum, grade) => sum + grade, 0) / student.grades.length;
+  
+  console.log(`Student: ${student.lastName} ${student.firstName}`);
+  console.log(`Number albumu: ${student.albumNumber}`);
+  console.log(`Oceny: ${student.grades.join(", ")}`);
+  console.log(`średnią ocena: ${averageGrade}`);
+  
+  // Zadanie 3-4
+  
+  let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2021,
+    color: "silver",
+    speed: 0,
+    birthYear: 2021,
+  
+    increaseSpeed: function (value) {
+      this.speed += value;
+    },
+  
+    decreaseSpeed: function (value) {
+      if (this.speed >= value) {
+        this.speed -= value;
+      } else {
+        this.speed = 0;
+      }
+    },
+  
+    getInfo: function () {
+      return `${this.brand} ${this.model} (${this.year}), Color: ${this.color}, Speed: ${this.speed}`;
+    },
+    getAge: function () {
+      let currentYear = new Date().getFullYear();
+      return currentYear - this.birthYear;
+    },
+  };
+  
+  console.log(car.getInfo());
+  car.increaseSpeed(50);
+  console.log(car.getInfo());
+  car.decreaseSpeed(20);
+  console.log(car.getInfo());
+  car.decreaseSpeed(40);
+  console.log(car.getInfo());
+  console.log(car.getAge());
+  
+  // Zadanie 5
+  
+  let rectangle = {
+    width: 5,
+    height: 7,
+  
+    getArea: function () {
+      return this.width * this.height;
+    },
+  
+    getPerimeter: function () {
+      return 2 * (this.width + this.height);
+    },
+  
+    isSquare: function () {
+      return this.width === this.height;
+    },
+  };
+  
+  console.log(rectangle.getArea());
+  console.log(rectangle.getPerimeter());
+  console.log(rectangle.isSquare());
