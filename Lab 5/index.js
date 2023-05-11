a82222eae6562a72f6f2f0f8eace1d49c1b099d3
Anastasiya <nastia.bel04@gmail.com>
2023-05-11 23:57:07 +0200

    lab5

--- Lab 5/index.js
+++ Lab 5/index.js
@@ -1,13 +1,13 @@
-// let wiek = prompt("Podaj swoj wiek: ");
-// if (wiek < 18) {
-//     alert("Jestes niepełnoletni");
-// } else {
-//     alert("Jestes pełnoletni");
-// }
+let wiek = prompt("Podaj swoj wiek: ");
+if (wiek < 18) {
+    alert("Jestes niepełnoletni");
+} else {
+    alert("Jestes pełnoletni");
+}
 
-// let temp_cel = Number(prompt('Give me temperature in Celsjusze'))
-// let temp_far = temp_cel + 273.5
-// console.log(temp_far)
+let temp_cel = Number(prompt('Give me temperature in Celsjusze'));
+let temp_far = temp_cel + 273.5;
+console.log(temp_far);
 
 // let tablica = [1, 23, 43, 4, 8, 5, 3, 6, 7, 7, '7'];
 // console.log(tablica.reduce((a, b) => a+b));
@@ -26,23 +26,6 @@
 // console.log(sr);
 // max = Math.max(...tablica)
 // console.log(max)
-// c
-// function Fibonacci(n) {
-//     let ciag = []
-//     if(n < 2) return n;
-//     let a = 0, b = 1;
-//     for (let i = 2; i <= n; i++) {
-//         b += a;
-//         a = b-a;
-//         ciag.push(b);
-        
-//     }
-//     console.log(ciag);
-//     return b;
-// }
-
-// for(i = 0; i < 100; i++) console.log(Fibonacci(i));
-
 
 let tablica = [1,2,3,4,5];
 let suma = tablica.reduce((a,b) => a+b);
@@ -61,3 +44,22 @@ console.log(maxEl);
 let szuk = Number(prompt("Jakiej liczby szukamy?"));
 ile = tablica.filter(i => i===szuk);
 console.log(ile);
+
+function Fibonacci(n) {
+    let ciag = [];
+    if(n < 2) return n;
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        b += a;
+        a = b-a;
+        ciag.push(b);
+        
+    }
+    console.log(ciag);
+    return b;
+}
+
+for(i = 0; i < 100; i++) console.log(Fibonacci(i));
+
+
+
