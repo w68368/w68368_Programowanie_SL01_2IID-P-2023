2f620012a5e41a0047b02c01965762b2b3a40c55
Anastasiya <nastia.bel04@gmail.com>
2023-05-17 20:35:40 +0200

    lab4

--- Lab 4/index.js
+++ Lab 4/index.js
@@ -1,104 +1,99 @@
 // Zadanie 1
 
 const book = {
-    title: "Harry Potter i kamień filozoficzny",
-    author: "Joannę Rowling",
-    year: 1997,
-  };
-  
-function getBookInfo(book) {
-    return book.title + "-" + book.author + "(" + book.year + ")";
-  }
-  
-const bookInfo = getBookInfo(book);
-  
-console.log(bookInfo);
-  
-   // Zadanie 2
-
-  const student = {
-    firstName: "A",
-    lastName: "B",
-    albumNumber: "68153",
-    grades: [4, 4, 5],
-  };
-  
-//   const averageGrade =
-//     student.grades.reduce((sum, grade) => sum + grade, 0) / student.grades.length;
-
-const averageGrade = (student.grades.reduce((a,b)=>a+b,0) )/student.grades.length;
-  
-console.log("Student: " + student.firstName + " " + student.lastName);
-console.log("Numer albomu: " + student.albumNumber);
-console.log("Oceny: " + student.grades);
-console.log("Srednia ocena: " + averageGrade);
-  
-  // Zadanie 3-4
-  
-  let car = {
-    brand: "Toyota",
-    model: "Camry",
-    year: 2021,
-    color: "silver",
-    speed: 0,
-    birthYear: 2021,
-    
-    increaseSpeed: function (value) {
-      this.speed += value;
-    },
-  
-    decreaseSpeed: function (value) {
-      if (this.speed >= value) {
-        this.speed -= value;
-      } else {
-        this.speed = 0;
-      }
-    },
-  
-    getInfo: function () {
-      return this.brand + " " + this.model + "(" + this.year + ")" + ", " + "Kolor: " + this.color + " " + "Predkosc: " + this.speed;
-    },
-    getAge: function () {
-      let currentYear = new Date().getFullYear();
-      return currentYear - this.birthYear;
-    },
-  };
-  
-  console.log(car.getInfo());
-  car.increaseSpeed(50);
-  console.log(car.getInfo());
-  car.decreaseSpeed(20);
-  console.log(car.getInfo());
-  car.decreaseSpeed(40);
-  console.log(car.getInfo());
-  console.log("Age: " + car.getAge());
-  
-  // Zadanie 5
-  
-  let wysota = Number(prompt("wysota prostakata: ", "0"));
-  let szer = Number(prompt("szerokosc prostakata: ", "0"));
-  let prost = {
-    height: wysota,
-    width: szer,
-
-    getArea: function(){
-        return this.height * this.width;
-    },
-    getPerimeter: function() {
-        return (this.height * 2) + (this.width * 2);
-    },
-    getSquare: function() {
-        if (this.height == this.width) {
-            return true;
-        } else {
-            return false;
-        }
-    },  
+  title: "Harry Potter i kamień filozoficzny",
+  author: "Joannę Rowling",
+  year: 1997,
 };
 
-document.write("Pole: " + prost.getArea() + " ");
-document.write("Obwod: " + prost.getPerimeter() + " ");
-document.write("Prostakat jest kwadratem: " + prost.getSquare());
+console.log(`${book.title} - ${book.author} (${book.year})`);
+
+// В  переменную  можно задать разные значения типа как автор год и т.д
+// Чтобы использовать что-то одно можно использовать просто название переменной и значение (book.title)
+// Но если надо совместить пару значений можно использовать такую функцию `${}`
+
+// Zadanie 2
+
+const student = {
+  firstName: "",
+  lastName: "",
+  albumNumber: "68153",
+  grades: [4, 4, 5],
+};
+
+const averageGrade =
+  student.grades.reduce((sum, grade) => sum + grade, 0) / student.grades.length; // lenght длина массива
+// reduce - применяется каждому элементу массива (слева-направо), возвращая одно результирующее значение.
+// grade - это текущий обрабатываемый элемент массива.
+// sum - перебирает каждый елемент массива. Все просто acc + curr. Это означает что каждый елемент массива будет добавляться
+
+console.log(`Student: ${student.lastName} ${student.firstName}`);
+console.log(`Number albumu: ${student.albumNumber}`);
+console.log(`Oceny: ${student.grades.join(", ")}`);
+console.log(`średnią ocena: ${averageGrade}`);
+
+// Zadanie 3-4
+
+let car = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2021,
+  color: "silver",
+  speed: 0,
+  birthYear: 2021,
+
+  increaseSpeed: function (value) {
+    this.speed += value;
+  },
+
+  decreaseSpeed: function (value) {
+    if (this.speed >= value) {
+      this.speed -= value;
+    } else {
+      this.speed = 0;
+    }
+  },
+
+  getInfo: function () {
+    return `${this.brand} ${this.model} (${this.year}), Color: ${this.color}, Speed: ${this.speed}`;
+  },
+  getAge: function () {
+    let currentYear = new Date().getFullYear();
+    return currentYear - this.birthYear;
+  },
+};
+
+console.log(car.getInfo());
+car.increaseSpeed(50);
+console.log(car.getInfo());
+car.decreaseSpeed(20);
+console.log(car.getInfo());
+car.decreaseSpeed(40);
+console.log(car.getInfo());
+console.log(car.getAge());
+
+// Zadanie 5
+
+let rectangle = {
+  width: 5,
+  height: 7,
+
+  getArea: function () {
+    return this.width * this.height;
+  },
+
+  getPerimeter: function () {
+    return 2 * (this.width + this.height);
+  },
+
+  isSquare: function () {
+    return this.width === this.height;
+  },
+};
+
+console.log(rectangle.getArea());
+console.log(rectangle.getPerimeter());
+console.log(rectangle.isSquare());
 
 
 
