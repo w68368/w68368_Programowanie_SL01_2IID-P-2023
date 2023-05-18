82498588b6d1c53f8b26fc1613066eb09563cd1e
Anastasiya <nastia.bel04@gmail.com>
2023-05-19 00:15:24 +0200

    lab9

--- Lab 9/index.js
+++ Lab 9/index.js
@@ -7,17 +7,21 @@
 // }
 
 let activeBtn;
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-btn1.addEventListener('click', () => handleClick(btn1));
-btn2.addEventListener('click', () => handleClick(btn2));
-btn3.addEventListener('click', () => handleClick(btn3));
+      const btn1 = document.querySelector("#btn1");
+      const btn2 = document.querySelector("#btn2");
+      const btn3 = document.querySelector("#btn3");
 
-function handleClick(btn) {
-    if(activeBtn) {
-        activeBtn.classList.remove('active');
-    }
-    btn.classList.add('active');
-    activeBtn = btn;
-}
+      console.log(btn1);
+      btn1.addEventListener("click", () => handleClick(btn1));
+      btn2.addEventListener("click", () => handleClick(btn2));
+      btn3.addEventListener("click", () => handleClick(btn3));
+
+      function handleClick(btn) {
+        if (activeBtn) {
+          activeBtn.classList.remove("active");
+        }
+
+        btn.classList.add("active");
+        activeBtn = btn;
+      }
+
